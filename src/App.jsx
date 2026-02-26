a079a6c3000b61e40fc5440109f0582ff2652c4b
eigengrau01 <bryantroncosob@gmail.com>
2026-02-26 12:01:27 -0300

    feat: home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Auth from "./pages/Auth"
-import Checkout from "./pages/Checkout"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
 
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <Routes>
-	<Route path="/" element={<Home/>} />
-	<Route path="/auth" element={<Auth/>} />
-	<Route path="/checkout" element={<Checkout/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,3 +1,3 @@
 export default function Auth() {
-  return <div>Auth Page</div>
+  return <div>Auth Page</div>;
 }
