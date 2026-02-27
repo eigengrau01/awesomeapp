1258845fa7a4f5da08d714bebe5d1748b24dac9f
eigengrau01 <bryantroncosob@gmail.com>
2026-02-27 19:59:42 -0300

    feat: auth added

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const AuthContext = createContext(null);
+
+export default function AuthProvider({ children }) {
+  const { user, setUser } = useState(null);
+
+  function signUp() {}
+
+  function login() {}
+
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+}
